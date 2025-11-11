50e2e257d1d4b6280b5ac9b12d0ed2004e7d9d16
JKdouless <notebookdetrabajo@gmail.com>
2025-11-11 19:30:38 -0300

    funkar

--- visualizador/algorithms/sort_selection.py
+++ visualizador/algorithms/sort_selection.py
@@ -1,12 +1,10 @@
-# Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
-
 items = []
 n = 0
-i = 0          # cabeza de la parte no ordenada
-j = 0          # cursor que recorre y busca el mínimo
-min_idx = 0    # índice del mínimo de la pasada actual
-fase = "buscar"  # "buscar" | "swap"
-a=0
+i = 0
+j = 0
+min_idx = 0
+fase = "buscar"
+
 def init(vals):
     global items, n, i, j, min_idx, fase
     items = list(vals)
@@ -16,42 +14,30 @@ def init(vals):
     min_idx = i
     fase = "buscar"
 
-#def step():
-    # TODO:
-    # - Fase "buscar": comparar j con min_idx, actualizar min_idx, avanzar j.
-    #   Devolver {"a": min_idx, "b": j_actual, "swap": False, "done": False}.
-    #   Al terminar el barrido, pasar a fase "swap".
-    # - Fase "swap": si min_idx != i, hacer ese único swap y devolverlo.
-    #   Luego avanzar i, reiniciar j=i+1 y min_idx=i, volver a "buscar".
-    #
-    # Cuando i llegue al final, devolvé {"done": True}.
-    #return {"done": True}
-
-
-
 def step():
     global items, n, i, j, min_idx, fase
     if i >= n - 1:
         return {"done": True}
+        
+
     if fase == "buscar":
         if j < n:
             if items[j] < items[min_idx]:
                 min_idx = j
             j_actual = j
             j += 1
-            return {"a": min_idx, "b": j_actual, "swap": True, "done": False}
-        if i >= n - 1:
-            return {"done": True}       
-        fase = "swap"
-
+            return {"a": i, "b": j_actual, "swap": False}
+        else:
+            fase = "swap"
     if fase == "swap":
-            items[j], items[min_idx] = items[min_idx], items[j]
-            return {"a": i, "b": min_idx, "swap": True, "done": False}
-
-    i += 1
-    if i < n - 1:
-        min_idx = i
-        j = i + 1
-        fase = "buscar"
-    
-   
+        items[i], items[min_idx] = items[min_idx], items[i]
+        a, b = i, min_idx
+        i += 1
+        if i <= n - 1:
+            j = i + 1
+            min_idx = i
+            fase = "buscar"
+            return {"a": a, "b": b, "swap": True}
+        else:
+            return{"a": items[n-1],"b": items[n],"done": True}
+        
