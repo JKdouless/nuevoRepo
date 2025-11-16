2905b4dbb6daf9412170ace887629acf2141eeb3
JKdouless <notebookdetrabajo@gmail.com>
2025-11-16 20:03:15 -0300

    Update sort_shell.py

--- visualizador/algorithms/sort_shell.py
+++ visualizador/algorithms/sort_shell.py
@@ -1,86 +1,87 @@
 items = []
 n = 0
-
 gaps = []
 gap_index = 0
 gap = 0
-
 i = 0
 j = 0
-
 fase = "comparar"
-
 
 def init(vals):
     global items, n, gaps, gap_index, gap, i, j, fase
     items = list(vals)
     n = len(items)
-
-    gaps = [2, 1]           # ← lo que pediste
-    gap_index = 0
-    gap = gaps[gap_index]
-
+    gaps = [2, 1] #gaps posibles
+    gap_index = 0 #ind gap
+    gap = gaps[gap_index] #valor del gap actual
     i = gap
     j = i
-
     fase = "comparar"
-
 
 def step():
     global items, n, gaps, gap_index, gap, i, j, fase
-
-    # Si ya pasamos todos los gaps → terminado
     if gap_index >= len(gaps):
         return {"done": True}
-
     # Si el gap es mayor que n, saltamos al siguiente
     if gap >= n:
         gap_index += 1
         if gap_index >= len(gaps):
-            return {"done": True}
-        gap = gaps[gap_index]
-        i = gap
-        j = i
+            return {"done": True} # termino si no hay mas gaps
+        gap = gaps[gap_index] #q gap uso
+        i = gap #equivalgo gap a i para pasar
+        j = i #barredor
         fase = "comparar"
-        return {"a": j, "b": j-gap if j >= gap else j, "swap": False}
-
-    # --------------- FASE COMPARAR ----------------
+        if j >= gap:
+            b = j - gap
+        else:
+            b = j
+        return {"a": j, "b": b, "swap": False}
+    # ///////////////// comparar ///////////////////
     if fase == "comparar":
-
-        if j >= gap and items[j] < items[j - gap]:
+        if j >= gap and items[j] < items[j - gap]: #posicion en la sublista actual
             fase = "swap"
-            return {"a": j, "b": j - gap, "swap": False}
-
+            if j >= gap:
+                b = j - gap
+            else:
+                b = j
+            return {"a": j, "b": b, "swap": False}
         else:
             fase = "avanzar"
-            return {"a": j, "b": j - gap if j >= gap else j, "swap": False}
-
-    # --------------- FASE SWAP --------------------
+            if j >= gap:
+                b = j - gap
+            else:
+                b = j
+            return {"a": j, "b": b, "swap": False}
+    # /////////////// swap ///////////////
     if fase == "swap":
-        items[j], items[j - gap] = items[j - gap], items[j]
+        items[j], items[j - gap] = items[j - gap], items[j] #intercambio con en la sublista del swap
         a, b = j, j - gap
         j -= gap
         fase = "comparar"
         return {"a": a, "b": b, "swap": True}
-
-    # --------------- FASE AVANZAR -----------------
+    # /////////////// avanzo ///////////////////
     if fase == "avanzar":
         i += 1
         if i < n:
             j = i
             fase = "comparar"
-            return {"a": j, "b": j - gap if j >= gap else j, "swap": False}
-
+            if j >= gap:
+                b = j - gap
+            else:
+                b = j
+            return {"a": j, "b": b, "swap": False}
         else:
-            # Terminado este gap → pasar al siguiente
+            # cambio de gap
             gap_index += 1
-
             if gap_index >= len(gaps):
                 return {"done": True}
-
             gap = gaps[gap_index]
             i = gap
             j = i
             fase = "comparar"
-
-            return {"a": j, "b": j - gap if j >= gap else j, "swap": False}
+            if j >= gap:
+                b = j - gap
+            else:
+                b = j
+            return {"a": j, "b": b, "swap": False}
+    # ///////////// terminatres ///////////////////////
